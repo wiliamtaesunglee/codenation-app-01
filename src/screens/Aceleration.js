31d26cb8bb0500cf32aeff817800fc4fa500a3e7
wil <wil@wil.i.am>
2020-03-01 20:43:54 -0300

    course content added

--- src/screens/Aceleration.js
+++ src/screens/Aceleration.js
@@ -1,24 +1,47 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 const Aceleration = () =>{
     const [results, setResults] = useState([]);
 
-	(async () => await fetch("http://localhost:8081/data.json")
+    useEffect
+    async () => await fetch("http://localhost:8081/data.json")
             .then(r => r.json())
             .then(r => setResults(r))
-    )();
+            .then(console.log(results))
 
     return(
-        <View>
-            <Text>Aceleraćões</Text>
-            
+        <View style={styles.container}>
+            <Image styles={styles.image} source={require('../../assets/images/100.png')}/>
+            <View style={styles.infos}>
+                <Text style={styles.title} >React Native</Text>
+                <Text>Online</Text>
+                <Text>20/12/2022</Text>
+            </View>
         </View>
     )
     
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 20,
+        marginLeft: 45,
+        flexDirection: 'row',
+        width: 400,
+        
+    },
+    image: {
+        width: 100
+    },
+    title: {
+        fontSize: 20
+    },
+    infos: {
+        marginLeft: 30,
+        justifyContent: 'space-around'
+    }
+});
 
 export default Aceleration;
 
